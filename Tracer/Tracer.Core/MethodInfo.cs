20b7719ad60107a14b56e6699af5bc82abf0b1f9
North0n <mautin.ivan.3@gmail.com>
2022-09-03 23:59:58 +0300

    Add MethodInfo class
    It represents info about particular method and contains methods, invoked from it

---
+++ Tracer/Tracer.Core/MethodInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace Tracer.Core;
+
+public class MethodInfo
+{
+    public MethodInfo(string name, string @class, int ms)
+    {
+        Name = name;
+        Class = @class;
+        Milliseconds = ms;
+    }
+
+    public string Name { get; }
+    public string Class { get; }
+    internal long Milliseconds { get; set; }
+    public string Time => $"{Milliseconds}ms";
+    public List<MethodInfo> Methods { get; } = new();
+}
