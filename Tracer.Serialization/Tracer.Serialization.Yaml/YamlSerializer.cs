c8268cbee1caa0e343de88580c2121de5ef8e0eb
North0n <mautin.ivan.3@gmail.com>
2022-09-05 00:06:37 +0300

    Add Yaml serialization

---
+++ Tracer.Serialization/Tracer.Serialization.Yaml/YamlSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+using YamlDotNet.Serialization;
+
+namespace Tracer.Serialization.Yaml;
+
+public class YamlSerializer : ITraceResultSerializer
+{
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        var serializer = new SerializerBuilder().DisableAliases().Build();
+        var result = serializer.Serialize(traceResult);
+        to.Write(Encoding.UTF8.GetBytes(result));
+    }
+}
