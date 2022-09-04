2bbcbfe8b839fd3714a5b1c6b2a972301b3df0b7
North0n <mautin.ivan.3@gmail.com>
2022-09-04 20:02:58 +0300

    Add JSON serializer

---
+++ Tracer.Serialization/Tracer.Serialization.Json/JsonSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+
+namespace Tracer.Serialization.Json;
+
+public class JsonSerializer : ITraceResultSerializer
+{
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        var res = System.Text.Json.JsonSerializer.Serialize<TraceResult>(traceResult);
+        to.Write(Encoding.Default.GetBytes(res));
+    }
+}
